3a3c03ec597d8e9018fe44686c2b4286771b8e9e
dhani.057 <dhani.057@gmail.com>
2017-10-18 14:09:09 +0700

    + Add Something

--- WebTemplate/Controllers/SettlementController.cs
+++ WebTemplate/Controllers/SettlementController.cs
@@ -30,5 +30,10 @@ namespace WebTemplate.Controllers
         {
             return View();
         }
+
+        public ActionResult NotaSettlement()
+        {
+            return View();
+        }
     }
 }
